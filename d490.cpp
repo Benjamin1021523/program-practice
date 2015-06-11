5e36cc1289208177a8fae905ef8b138c6d87d110
s1021523 <benjamin.30805@yahoo.com.tw>
2015-06-11 09:40:23 +0000

    區間偶數和

---
+++ d490.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long int a, b, c;
+    while(cin >> a >> b){
+        if(a < b && a % 2 == 1){
+            a++;
+        }
+        if(a < b && b % 2 == 1){
+            b--;
+        }
+        if(a == b && a % 2 == 0){
+            c = a;
+        }
+        else if(a == b && a % 2 == 1){
+            c = 0;
+        }
+        else{
+            c = (a + b) * ((b - a) / 2 + 1) / 2;
+        }
+        cout << c << endl;
+    }
+}
