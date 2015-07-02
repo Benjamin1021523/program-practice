0f3d79b4540fd1e6dd66c4c3845cc18691093dd0
s1021523 <benjamin.30805@yahoo.com.tw>
2015-07-02 03:55:49 +0000

    very easy

---
+++ a861.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long int a, b, output;
+    while(cin >> a >> b){
+        output = 2 * (a + b);
+        cout << output << endl;
+    }
+}
